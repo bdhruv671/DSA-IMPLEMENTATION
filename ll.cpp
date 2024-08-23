017246e4f0de7b3c23868675f436e3fc9f4e5113
Dhruv Bisht <bdhruv671@gmail.com>
2024-08-24 00:02:53 +0530

    code

--- ll.cpp
+++ ll.cpp
@@ -1,12 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 struct Node{
-int data;
-Node *next;
-Node(int x){
+ int data;
+ Node *next ;
+ Node (int x){
     data = x;
-    next = NULL ;
-}
+    next = NULL;
+
+ }
 
 };
 
