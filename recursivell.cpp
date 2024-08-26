8df5ea36458e917e920e49580f6c3792fc9f895e
Dhruv Bisht <bdhruv671@gmail.com>
2024-08-26 15:48:44 +0530

    code

--- recursivell.cpp
+++ recursivell.cpp
@@ -10,12 +10,41 @@ struct Node {
         data = x;
         next = NULL;
     }
+};
+int search(Node* head ,int x){
+    int pos = 1;
+    Node*curr = head;
+    while(curr != NULL){
+        if (curr -> data ==x)
+        return pos ;
+
+     else {
+        pos++ ;
+        curr = curr-> next;
+
+    }
+    }
+   return -1;
+
+     
+
 }
 
 int main (){
     int t = 30 ;
-    int a =30;
+    // int a =30;
     Node *head = new Node(10);
-    Node *head -> next = new Node(30);
+    head -> next = new Node(20);
+    head -> next->next = new Node(30);
+     // Call search and store the result
+    int position = search(head, t);
+
+    if (position != -1) {
+        cout << "Element found at position: " << position << endl;
+    } else {
+        cout << "Element not found." << endl;
+    }
+
+    return 0;
 
 }
